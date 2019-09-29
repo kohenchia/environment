ffd3a2de5f33c600cb8003a410c971d25b3f9f71
Kohen Chia <kohenchia@gmail.com>
2019-09-29 07:35:45 -0700

    Fixing script and spacing

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 link_file() {
     if [ -f "${2}" ]; then
-	if [ -f "${2}.bak" ]; then
-	    unlink ${2}.bak
-	fi
-	mv ${2} ${2}.bak
+    if [ -f "${2}.bak" ]; then
+        unlink ${2}.bak
+    fi
+    mv ${2} ${2}.bak
     fi
     ln -s $(pwd)/${1} ${2}
 }
@@ -16,12 +16,12 @@ link_file vim/.gvimrc ~/.gvimrc
 
 link_directory() {
     if [ -d "${2}" ]; then
-	if [ -d "${2}.bak" ]; then
-	    unlink ${2}.bak
-	fi
-	mv ${2} ${2}.bak
+    if [ -d "${2}.bak" ]; then
+        unlink ${2}.bak
+    fi
+    mv ${2} ${2}.bak
     fi
     ln -s $(pwd)/${1} ${2}
 }
 
-link_directory vim .vim
+link_directory vim ~/.vim
